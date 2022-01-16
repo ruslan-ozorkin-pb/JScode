91682ba7244d247c91f23649934eb4fe07cf96a1
Ruslan Ozorkin <ruslan.ozerkin@privatbank.ua>
2022-01-16 16:51:49 +0200

    готово

--- hw1/task1.js
+++ hw1/task1.js
@@ -1,4 +1,16 @@
+// Перепишите код, заменив оператор `if` на тернарный оператор `?`
+
+// ```js
+// var result;
+// if (a + b < 4) {
+// result = true;
+// } else {
+// result = false;
+// }
+// ```
+
 var result;
 var a = 1;
-var b =2;
-if (a + b < 4)  true , false;
+var b = 2;
+result = (a+b <4) ? true:false;
+
---
+++ hw1/task6.js
@@ -0,0 +1,6 @@
+var arr = [1,2,3,4];
+var sum =0;
+for (var i = 0; i < arr.length; i++) {
+    var sum = sum + arr[i];
+    }
+    console.log(sum);
