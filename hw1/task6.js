422ed7bdde589ce3a755a145f10b0775166b1a0c
Ruslan Ozorkin <ruslan.ozerkin@privatbank.ua>
2022-01-18 21:07:46 +0200

    hw1- 6 recode

--- hw1/task6.js
+++ hw1/task6.js
@@ -1,6 +1,7 @@
 var arr = [1,2,3,4];
 var sum =0;
-for (var i = 0; i < arr.length; i++) {
+var x = arr.length;
+for (var i = 0; i < x; i++) {
     var sum = sum + arr[i];
     }
     console.log(sum);
